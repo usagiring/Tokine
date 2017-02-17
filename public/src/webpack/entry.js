6770b8d9dff0fd014d638fd460186389a6b56269
hling51325 <hling51325@163.com>
2017-02-17 17:21:46 +0800

    build react

--- public/src/webpack/entry.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by RedMoon on 2017/2/14.
- */
-
-require("./style.css") // 载入 style.css
-document.write('It works.');
-document.write(require('./module.js'));
--- public/src/webpack/module.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by RedMoon on 2017/2/14.
- */
-
-module.exports = 'It works from module.js.';
