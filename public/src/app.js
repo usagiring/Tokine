c460e6187779230ba9639e2474774bc4d76f25e6
hling51325 <hling51325@163.com>
2017-02-17 18:46:51 +0800

    build react

--- public/src/app.js
+++ public/src/app.js
@@ -6,6 +6,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'scss/test.scss';
 import $ from 'jquery';
+import action from 'action';
+
+function todoApp(state = initialState, action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            })
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if(index === action.index) {
+                        return Object.assign({}, todo, {
+                            completed: !todo.completed
+                        })
+                    }
+                    return todo
+                })
+            })
+        default:
+            return state
+    }
+}
 
 ReactDOM.render(
     <h1>Hello, world!</h1>,
--- public/src/module/module.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by RedMoon on 2017/2/17.
- */
-
-module.exports = 'It works from module.js.';
