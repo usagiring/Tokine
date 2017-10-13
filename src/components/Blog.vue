9fd63c9bbb9df31d70e67127858e5821ab02e1ec
hling51325 <hling51325@163.com>
2017-10-13 17:23:01 +0800

    blogs and blog

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="blog">
+    <div v-html="blog.content"></div>
 
   </div>
 </template>
 
 <script>
+  import {get} from '../utilities/rest'
+
   export default {
     name: 'blog',
+    created() {
+      let id = this.$route.params.id
+      let url = `/blog/${id}`
+      get(url)
+        .then(blog => {
+          this.blog = blog
+        })
+    },
     data() {
       return {
+        blog: {}
       }
     },
-    computed: {
-    }
+    computed: {}
   }
 </script>
 
--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="blogs">
+    <div>
+      <div v-for="blog in blogs">
+        <hr>
+        <span @click="blogDetail(blog)">{{blog.title}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import {get} from '../utilities/rest'
+
   export default {
     name: 'blogs',
     data() {
       return {
+        blogs: []
       }
     },
-    computed: {
+    created() {
+      let url = '/blogs';
+      get(url)
+        .then(blogs => {
+          this.blogs = blogs
+        })
+    },
+    computed: {},
+    methods: {
+      blogDetail(blog) {
+        this.$router.push(`/blog/${blog._id}`)
+      }
     }
   }
 </script>
